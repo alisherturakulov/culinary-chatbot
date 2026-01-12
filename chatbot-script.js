af34d08459f6b5498e5a29256aca72e478a59cb8
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-01-12 09:23:13 -0500

    write toggleChatBox function and add event listener to chat tab

--- chatbot-script.js
+++ chatbot-script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+//Script for chatbot
+
+console.log("Hello World");
+
+const chatBoxTab = document.getElementById("chatBotTab");
+chatBoxTab.addEventListener("click", toggleChatBox);
+
+
+//function definitions
+
+/**
+ * adds/removes the openedChatBox class from styles to make the chatBox
+ * visible or not
+ */
+function toggleChatBox(){
+    const chatBox = document.getElementById("chatBotBox");
+    chatBox.classList.toggle("openedChatBox");
+    console.log("openedChatBox");
+}
+
