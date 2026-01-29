87ffa8d40e2de980b353e16a1745eab3432eb0cf
Alisherjon Turakulov <aturakulov100@gmail.com>
2026-01-29 18:01:52 -0500

    create plugin folder with php file

---
+++ culinary-chatbot-plugin/culinary-chatbot-plugin.php
@@ -0,0 +1,9 @@
+/*
+ * Plugin Name:       Culnary Argan Oil Chatbot
+ * Plugin URI:        https://alisherturakulov.github.io/culinary-chatbot
+ * Description:       Adds a chatbot to the Culinary Argan Oil website pages
+ * Author:            Alisherjon Turakulov
+ * Author URI:        https://github.com/alisherturakulov
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
